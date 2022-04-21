5fc62afeb7bc5948a2a94cbdb1489d080ab1f43c
nehachougule1998 <nchougule734@gmail.com>
2022-04-21 14:40:52 +0530

    Added

---
+++ CaseStatements/NumberInWord.js
@@ -0,0 +1,41 @@
+
+let number = Math.round(Math.random() * 10 + 1);
+switch (number) {
+    case 1:
+        console.log("Number in word = ONE");
+        break;
+    case 2:
+        console.log("Number in word = TWO");
+        break;
+
+    case 3:
+        console.log("Number in word= THREE");
+        break;
+
+    case 4:
+        console.log("Number in word= FOUR");
+        break;
+
+    case 5:
+        console.log("Number in word= FIVE");
+        break;
+
+    case 6:
+        console.log("Number in word = SIX");
+        break;
+
+    case 7:
+        console.log("Number in word = SEVEN");
+        break;
+
+    case 8:
+        console.log("Number in word = EIGHT");
+        break;
+
+    case 9:
+        console.log("Number in word= NINE");
+        break;
+
+    default:
+        console.log("Invalid Number");
+}
